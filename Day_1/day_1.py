e1333630b5cd90ebca0cf5499c0bf49b0927bcb2
Edward Chamberlain <edward.chamberlain@me.com>
2020-12-01 20:10:42 +0000

    Updated to use itertools

--- Day_1/day_1.py
+++ Day_1/day_1.py
@@ -1,3 +1,7 @@
+import itertools
+
+RESULT = 2020
+
 # Open Data
 with open("Day_1/input_1.txt", 'r') as f:
     data = f.readlines()
@@ -5,9 +9,16 @@ with open("Day_1/input_1.txt", 'r') as f:
 # Convert to int
 data = [int(i.strip()) for i in data]
 
-# Loop over looking for sum = 2020
-for v in data:
-    for t in data:
-        if v + t == 2020:
-            print(f"{v} + {t} = 2020!\n{v} x {t} = {v * t}")
-            exit()
+
+# Search for pairs summing RESULT
+results = [(x, y) for x, y in itertools.combinations(data, 2) if x + y == RESULT]
+
+for i in results:
+    print(f"{i[0]} + {i[1]} = {RESULT}!\n{i[0]} x {i[1]} = {i[0] * i[1]}", end='\n\n')
+
+
+# Search for tripple summing RESULT
+results = [(x, y, z) for x, y, z in itertools.combinations(data, 3) if x + y + z == RESULT]
+
+for i in results:
+    print(f"{i[0]} + {i[1]} + {i[2]} = {RESULT}!\n{i[0]} x {i[1]} x {i[2]}= {i[0] * i[1] * i[2]}", end='\n\n')
