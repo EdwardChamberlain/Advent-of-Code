a8b0be649f92a6890b3798d6e02bde895cf6917d
Edward Chamberlain <edward.chamberlain@me.com>
2020-12-01 20:17:41 +0000

    applied PEP8

--- Day_1/day_1.py
+++ Day_1/day_1.py
@@ -11,14 +11,21 @@ data = [int(i.strip()) for i in data]
 
 
 # Search for pairs summing RESULT
-results = [(x, y) for x, y in itertools.combinations(data, 2) if x + y == RESULT]
-
+results = [(x, y) for x, y in itertools.combinations(data, 2) 
+                if x + y == RESULT]
 for i in results:
-    print(f"{i[0]} + {i[1]} = {RESULT}!\n{i[0]} x {i[1]} = {i[0] * i[1]}", end='\n\n')
-
+    print(
+        f" {i[0]} + {i[1]} = {RESULT}!\n"
+        f" {i[0]} x {i[1]} = {i[0] * i[1]}",
+        end='\n\n'
+        )
 
 # Search for tripple summing RESULT
-results = [(x, y, z) for x, y, z in itertools.combinations(data, 3) if x + y + z == RESULT]
-
+results = [(x, y, z) for x, y, z in itertools.combinations(data, 3)
+                if x + y + z == RESULT]
 for i in results:
-    print(f"{i[0]} + {i[1]} + {i[2]} = {RESULT}!\n{i[0]} x {i[1]} x {i[2]}= {i[0] * i[1] * i[2]}", end='\n\n')
+    print(
+        f" {i[0]} + {i[1]} + {i[2]} = {RESULT}!\n",
+        f"{i[0]} x {i[1]} x {i[2]}= {i[0] * i[1] * i[2]}",
+        end='\n\n'
+        )
