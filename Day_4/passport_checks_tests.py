8b65a274c193ea3b63a916d75e09d367dc896179
Edward Chamberlain <edward.chamberlain@me.com>
2020-12-04 16:24:18 +0000

    updated to use unittest

--- Day_4/passport_checks_tests.py
+++ Day_4/passport_checks_tests.py
@@ -1,19 +1,29 @@
+import unittest
 import passport_checks
 
-print(True == passport_checks.check_birth(str(2002)))
-print(False == passport_checks.check_birth(str(2003)))
+class test_passport_methods(unittest.TestCase):
+    def test_birth(self):
+        self.assertTrue(passport_checks.check_birth('2002'))
+        self.assertFalse(passport_checks.check_birth('2003'))
 
-print(True == passport_checks.check_height('60in'))
-print(True == passport_checks.check_height('190cm'))
-print(False == passport_checks.check_height('190in'))
-print(False == passport_checks.check_height('190'))
+    def test_height(self):
+        self.assertTrue(passport_checks.check_height('60in'))
+        self.assertTrue(passport_checks.check_height('190cm'))
+        self.assertFalse(passport_checks.check_height('190in'))
+        self.assertFalse(passport_checks.check_height('190'))
 
-print(True == passport_checks.check_hair('#123abc'))
-print(False == passport_checks.check_hair('#123abz'))
-print(False == passport_checks.check_hair('123abc'))
+    def test_hair(self):
+        self.assertTrue(passport_checks.check_hair('#123abc'))
+        self.assertFalse(passport_checks.check_hair('#123abz'))
+        self.assertFalse(passport_checks.check_hair('123abc'))
 
-print(True == passport_checks.check_eye('brn'))
-print(False == passport_checks.check_eye('wat'))
+    def test_eye(self):
+        self.assertTrue(passport_checks.check_eye('brn'))
+        self.assertFalse(passport_checks.check_eye('wat'))
 
-print(True == passport_checks.check_id('000000001'))
-print(False == passport_checks.check_id('0123456789'))
+    def test_id(self):
+        self.assertTrue(passport_checks.check_id('000000001'))
+        self.assertFalse(passport_checks.check_id('0123456789'))
+
+if __name__ == '__main__':
+    unittest.main()
