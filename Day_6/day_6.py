ce5c2d20f48d16a6632132108cb41af76ddde4e5
Edward Chamberlain <edward.chamberlain@me.com>
2020-12-07 22:18:05 +0000

    pt 2

--- Day_6/day_6.py
+++ Day_6/day_6.py
@@ -3,11 +3,16 @@ import question_parser
 
 with open("Day_6/input_6.txt", 'r') as f:
     data = f.read().split('\n\n')
-
 data = [i.split('\n') for i in data]
 
 totals = []
 for i in data:
     totals.append(question_parser.or_resp(i))
 
-print(sum(totals))
+print(f"Total pt1: {sum(totals)}")
+
+totals = []
+for i in data:
+    totals.append(question_parser.count_response2(i))
+
+print(f"Total pt2: {sum(totals)}")
