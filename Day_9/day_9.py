ff811c27df76bb94d6c6adcff4a86266c85d1cfa
Edward Chamberlain <edward.chamberlain@me.com>
2020-12-09 10:44:16 +0000

    Day 9

--- Day_9/day_9.py
+++ Day_9/day_9.py
@@ -10,7 +10,6 @@ data = [int(i) for i in data]
 invalid_value = encoder.validate_list(data, PREAMBLE)
 print(f"Invalid Value = {invalid_value}")
 
-
 data_range = encoder.find_range(data, invalid_value)
 result = min(data_range) + max(data_range)
 print(f"Pt 2 Result: {result}")
