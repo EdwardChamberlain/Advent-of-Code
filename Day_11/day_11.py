d95906dbf6ec9ae6a8a6079b1c038249afcae6aa
Edward Chamberlain <edward.chamberlain@me.com>
2020-12-12 22:33:25 +0000

    Pt 2

--- Day_11/day_11.py
+++ Day_11/day_11.py
@@ -14,5 +14,20 @@ while seat_parser.seats_in_use(seat_plan) != prev_seats:
     seat_plan = seat_parser.calc_new_seat_plan(seat_plan)
     loop_counter += 1
 
-print(f"Loops Taken: {loop_counter}")
-print(f"Total seats in use: {prev_seats}")
+print(f"Pt 1: Loops Taken: {loop_counter}")
+print(f"Pt 1: Total seats in use: {prev_seats}")
+
+# ~~~~~~~~~~~~~~~~~ Pt 2 ~~~~~~~~~~~~~~~~~~~~~~
+
+seat_plan = data
+
+loop_counter = 0
+prev_seats = 50
+
+while seat_parser.seats_in_use(seat_plan) != prev_seats:
+    prev_seats = seat_parser.seats_in_use(seat_plan)
+    seat_plan = seat_parser.calc_new_seat_plan2(seat_plan)
+    loop_counter += 1
+
+print(f"Pt 2: Loops Taken: {loop_counter}")
+print(f"Pt 2: Total seats in use: {prev_seats}")
--- Day_11/seat_parser.py
+++ Day_11/seat_parser.py
@@ -18,14 +18,6 @@ def get_connected_coords(coord, data):
     result = [i for i in result if not i[0] >= len(data) and not i[1] >= len(data[1])]
     return result
 
-def update_seat(seat, neighbours):
-    if seat == 'L' and neighbours['#'] == 0:
-        return '#'
-    elif seat == '#' and neighbours['#'] >= 4:
-        return 'L'
-    else:
-        return seat
-
 def seat_state_update(x, y, data):
     target_seat = data[x][y]
 
@@ -36,8 +28,13 @@ def seat_state_update(x, y, data):
 
     return update_seat(target_seat, neighbours_col)
 
-def calc_new_seat_plan(data):
-    return [[seat_state_update(x, y, data) for y, y_data in enumerate(x_data)] for x, x_data in enumerate(data)]
+def update_seat(seat, neighbours):
+    if seat == 'L' and neighbours['#'] == 0:
+        return '#'
+    elif seat == '#' and neighbours['#'] >= 4:
+        return 'L'
+    else:
+        return seat
 
 def seats_in_use(seating_plan):
     acc = 0
@@ -46,3 +43,53 @@ def seats_in_use(seating_plan):
             if y == '#':
                 acc += 1
     return acc
+
+def calc_new_seat_plan(data):
+    return [[seat_state_update(x, y, data) for y, y_data in enumerate(x_data)] for x, x_data in enumerate(data)]
+
+def add_vect(a, b):
+    return (a[0] + b[0], a[1] + b[1])
+
+def is_valid_location(coord, data):
+    return 0 <= coord[0] < len(data) and 0 <= coord[1] < len(data[0])
+
+def get_seat_in_direction(start, vect, data):
+    while True:
+        start = add_vect(start, vect)
+        if not is_valid_location(start, data):
+            break
+        x, y = start
+        if data[x][y] == 'L' or data[x][y] == '#':
+            return x, y
+    return None
+
+def get_connected_seats(coord, data):
+    vects = (
+        (1, 0), # Below
+        (-1, 0), # Above
+
+        (0, 1), # Right
+        (0, -1), # Left
+
+        (1, 1), # up-right
+        (1, -1), # up-left
+        (-1, 1), # down-right
+        (-1, -1), # down-left
+    )
+    result = [get_seat_in_direction(coord, v, data) for v in vects if get_seat_in_direction(coord, v, data) is not None]
+    return collections.Counter([data[i[0]][i[1]] for i in result])
+
+def update_seat2(seat, neighbours):
+    if seat == 'L' and neighbours['#'] == 0:
+        return '#'
+    elif seat == '#' and neighbours['#'] >= 5:
+        return 'L'
+    else:
+        return seat
+
+def seat_state_update2(x, y, data):
+    target_seat = data[x][y]
+    return update_seat2(target_seat, get_connected_seats((x, y), data))
+
+def calc_new_seat_plan2(data):
+    return [[seat_state_update2(x, y, data) for y, y_data in enumerate(x_data)] for x, x_data in enumerate(data)]
