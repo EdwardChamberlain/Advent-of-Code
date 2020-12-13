0c3e5ebb670061f3f8e038b47bbd743bb13344d8
Edward Chamberlain <edward.chamberlain@me.com>
2020-12-13 21:24:00 +0000

    Day 13 Pt 2

--- Day_13/day_13.py
+++ Day_13/day_13.py
@@ -1,13 +1,15 @@
 import math
+import string
+from sympy.ntheory.modular import crt 
 
 with open("Day_13/input_13.txt", 'r') as f:
     data = [i.strip() for i in f.readlines()]
-data[1] = [int(i) for i in data[1].split(',') if i != 'x']
 
+buses = [int(i) for i in data[1].split(',') if i != 'x']
 START_TIME = int(data[0])
 
 results = {}
-for i in data[1]:
+for i in buses:
     next_time = math.ceil(START_TIME/i)
     results[i] = next_time * i
 
@@ -17,3 +19,14 @@ time_to_wait = results[earliest_bus] - START_TIME
 result = time_to_wait * earliest_bus
 
 print(f"Pt 1: {result}")
+
+# ~~~~~~~~~~ Pt 2 ~~~~~~~~~~
+
+deltas = {int(b):i for i, b in enumerate(data[1].split(',')) if b != 'x'}
+
+m = [i for i in deltas]
+v = [m[i] - d for i, d in enumerate(deltas.values())]
+v[0] = 0
+
+result = crt(m, v)
+print(f"Pt 2: {result[0]}")
