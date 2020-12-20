b168002b1a5993ab914ab5a0d4f9712eb2e0aba0
Edward Chamberlain <edward.chamberlain@me.com>
2020-12-20 02:18:43 +0000

    Day 19 Pt 2

--- Day_19/day_19.py
+++ Day_19/day_19.py
@@ -1,5 +1,5 @@
 import message_parser
-
+import re
 
 with open("Day_19/input_19.txt", 'r') as f:
     data = [i.split('\n') for i in f.read().split('\n\n')]
@@ -9,9 +9,9 @@ for i in data[0]:
     split = i.split(': ')
     rules[int(split[0])] = split[1]
 
-# regex = message_parser.rec_resolve_rule(rules[0], rules, specials=False)
-# result = [message_parser.is_complient(i, regex) for i in data[1]]
-# print(f"Pt 1: {sum(result)}")
+regex = message_parser.rec_resolve_rule(rules[0], rules, specials=False)
+result = [message_parser.is_complient(i, regex) for i in data[1]]
+print(f"Pt 1: {sum(result)}")
 
 
 # ~~~~~~~~~~ Pt 2 ~~~~~~~~~~
@@ -20,6 +20,5 @@ rules[8] = '42 | 42 8'
 rules[11] = '42 31 | 42 11 31'
 
 regex = message_parser.rec_resolve_rule(rules[0], rules, specials=True)
-print(regex)
 result = [i for i in data[1] if message_parser.is_complient(i, regex)]
 print(f"Pt 2: {len(result)}")
