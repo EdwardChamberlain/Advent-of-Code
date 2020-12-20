dea87936cd4b4168eae353baf3157b28743b7f8b
Edward Chamberlain <edward.chamberlain@me.com>
2020-12-20 01:17:52 +0000

    Pt 2

--- Day_19/day_19.py
+++ Day_19/day_19.py
@@ -9,6 +9,16 @@ for i in data[0]:
     split = i.split(': ')
     rules[int(split[0])] = split[1]
 
-regex = message_parser.rec_resolve_rule(rules[0], rules)
+regex = message_parser.rec_resolve_rule(rules[0], rules, specials=False)
 result = [message_parser.is_complient(i, regex) for i in data[1]]
 print(f"Pt 1: {sum(result)}")
+
+
+# ~~~~~~~~~~ Pt 2 ~~~~~~~~~~
+
+rules[8] = '42 | 42 8'
+rules[11] = '42 31 | 42 11 31'
+
+regex = message_parser.rec_resolve_rule(rules[0], rules, specials=True)
+result = [i for i in data[1] if message_parser.is_complient(i, regex)]
+print(f"Pt 2: {len(result)}")
