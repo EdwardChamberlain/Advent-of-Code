defe1a43f3707ba555b756e508a0b9b568ad0bc3
Edward Chamberlain <edward.chamberlain@me.com>
2022-05-01 12:33:52 +0100

    Day 5 update

--- 2020/Day_05/Day_5.py
+++ 2020/Day_05/Day_5.py
@@ -1,6 +1,6 @@
 import binary_parser
 
-with open("Day_5/input_5.txt", 'r') as f:
+with open("2020/Day_05/input_5.txt", 'r') as f:
     data = f.readlines()
 
 seat_ids = []
@@ -8,11 +8,12 @@ for s in data:
     seat_loc = binary_parser.parse_seat(s.strip())
     seat_id = binary_parser.get_seat_id(seat_loc)
     seat_ids.append(seat_id)
+print(f"Max Seat ID: {max(seat_ids)}")
 
 prev = False
 for i in sorted(seat_ids):
     if prev:
         if i - 1 != prev:
-            print("Missing!", i - 1)
-
+            print("Missing Seat:", i - 1)
+            break
     prev = i
