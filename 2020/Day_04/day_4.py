5e367b19e6a1ce28c6086e094d4b7e333d803cb9
Edward Chamberlain <edward.chamberlain@me.com>
2022-05-01 12:28:16 +0100

    Updated Day 4

--- 2020/Day_04/day_4.py
+++ 2020/Day_04/day_4.py
@@ -1,40 +1,45 @@
+from calendar import c
 import re
 import passport_checks
 
 
-with open("Day_4/input_4.txt", 'r') as f:
+with open("2020/Day_04/input_4.txt", 'r') as f:
     data = f.read().split('\n\n')
 
-data = [i.replace('\n', ' ') for i in data]
+data = [i.replace('\n', ' ').split() for i in data]
+data = [{n.split(":")[0]:n.split(":")[1] for n in i} for i in data]
 
 count = 0
 for i in data:
-    parsed = i.split()
-    parsed = {i.split(":")[0]:i.split(":")[1] for i in parsed}
+    if passport_checks.check_req_fields(i):
+        count += 1
 
-    if not passport_checks.check_req_fields(parsed):
+print(f"Valid Passports (required fields): {count}")
+
+count = 0
+for i in data:
+    if not passport_checks.check_req_fields(i):
         continue
     
-    if not passport_checks.check_birth(parsed['byr']):
+    if not passport_checks.check_birth(i['byr']):
         continue
 
-    if not passport_checks.check_issue(parsed['iyr']):
+    if not passport_checks.check_issue(i['iyr']):
         continue
 
-    if not passport_checks.check_expiry(parsed['eyr']):
+    if not passport_checks.check_expiry(i['eyr']):
         continue
 
-    if not passport_checks.check_height(parsed['hgt']):
-        print('hgt:', parsed['hgt'])
+    if not passport_checks.check_height(i['hgt']):
         continue
 
-    if not passport_checks.check_hair(parsed['hcl']):
+    if not passport_checks.check_hair(i['hcl']):
         continue
 
-    if not passport_checks.check_eye(parsed['ecl']):
+    if not passport_checks.check_eye(i['ecl']):
         continue
 
-    if not passport_checks.check_id(parsed['pid']):
+    if not passport_checks.check_id(i['pid']):
         continue
 
     count += 1
