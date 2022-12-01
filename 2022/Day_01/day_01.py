f33201661eea8fd912bda0dfc6eb9faf96e3f9e2
Edward Chamberlain <edward.chamberlain@me.com>
2022-12-01 10:06:55 +0000

    Day 1 solution

--- 2022/Day_01/day_01.py
+++ 2022/Day_01/day_01.py
@@ -1,7 +1,21 @@
+import advent_tools
+
 import sys
 import os
 
 with open(os.path.dirname(sys.argv[0]) + "/input.txt", 'r') as f:
-    data = f.readlines()
+    data = f.read()
+    data = data.split('\n\n') 
     data = [d.strip() for d in data]
+    data = [d.split('\n') for d in data]
+    data = [list(map(float, d)) for d in data]
 
+total = [sum(d) for d in data]
+
+# pt 1
+print(max(total))
+
+# pt 2
+print(
+    sum(sorted(total, reverse=True)[:3])
+)
