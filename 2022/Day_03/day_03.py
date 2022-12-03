620651453bbe1d51913a081f810dfe1bf160b504
Edward Chamberlain <edward.chamberlain@me.com>
2022-12-03 14:29:53 +0000

    Tidy up Day 03

--- 2022/Day_03/day_03.py
+++ 2022/Day_03/day_03.py
@@ -2,36 +2,39 @@ import sys
 import os
 import string
 
-# Get Data
-with open(os.path.dirname(sys.argv[0]) + "/input.txt", 'r') as f:
-    data = f.read()
-    data = data.split('\n') 
 
-# Pt 1
-priorities = []
-for d in data:
-    splitpoint = int(len(d) / 2)
-    a = d[:splitpoint]
-    b = d[splitpoint:]
-
-    for i in a:
-        if i in b:
-            priorities.append(string.ascii_letters.index(i) + 1)
-            break
-
-print(sum(priorities))
-
-# Pt 2
-def chunk_list(x: list, chunk_size: int) -> list: 
+def chunk_list(x: list, chunk_size: int) -> list:
     for i in range(0, len(x), chunk_size):
         yield x[i: i + chunk_size]
 
 
+def get_priority(item: str) -> int:
+    return string.ascii_letters.index(item) + 1
+
+
+# Get Data
+with open(os.path.dirname(sys.argv[0]) + "/input.txt", 'r') as f:
+    data = f.read()
+    data = data.split('\n')
+
+# Pt 1
+priorities = []
+for rucksack in data:
+    splitpoint = int(len(rucksack) / 2)
+    comp_1 = rucksack[:splitpoint]
+    comp_2 = rucksack[splitpoint:]
+
+    for item in comp_1:
+        if item in comp_2:
+            priorities.append(get_priority(item))
+            break
+print(sum(priorities))
+
+# Pt 2
 priorities = []
 for group in chunk_list(data, chunk_size=3):
-    for i in group[0]:
-        if i in group[1] and i in group[2]:
-            priorities.append(string.ascii_letters.index(i) + 1)
+    for item in group[0]:
+        if item in group[1] and item in group[2]:
+            priorities.append(get_priority(item))
             break
-
 print(sum(priorities))
