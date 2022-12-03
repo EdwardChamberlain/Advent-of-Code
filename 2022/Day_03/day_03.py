1b6f07a7aeb4b94ffa5d802493a5e051d42e1d07
Edward Chamberlain <edward.chamberlain@me.com>
2022-12-03 13:42:32 +0000

    Pt 2

--- 2022/Day_03/day_03.py
+++ 2022/Day_03/day_03.py
@@ -2,12 +2,10 @@ import sys
 import os
 import string
 
-import collections
-
+# Get Data
 with open(os.path.dirname(sys.argv[0]) + "/input.txt", 'r') as f:
     data = f.read()
     data = data.split('\n') 
-
 
 # Pt 1
 priorities = []
@@ -23,3 +21,17 @@ for d in data:
 
 print(sum(priorities))
 
+# Pt 2
+def chunk_list(x: list, chunk_size: int) -> list: 
+    for i in range(0, len(x), chunk_size):
+        yield x[i: i + chunk_size]
+
+
+priorities = []
+for group in chunk_list(data, chunk_size=3):
+    for i in group[0]:
+        if i in group[1] and i in group[2]:
+            priorities.append(string.ascii_letters.index(i) + 1)
+            break
+
+print(sum(priorities))
