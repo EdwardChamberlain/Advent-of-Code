9acfa15875cc197aec2cd918f793af53fcd8f2db
Edward Chamberlain <edward.chamberlain@me.com>
2022-12-03 13:22:49 +0000

    day 2 part 2

--- 2022/Day_02/day_02.py
+++ 2022/Day_02/day_02.py
@@ -19,6 +19,16 @@ LOSS = [
     ['A', 'Z'],
 ]
 
+DRAW = [
+    ['A', 'X'],
+    ['B', 'Y'],
+    ['C', 'Z'],
+]
+
+WIN_LOOKIP = dict(WIN)
+LOSS_LOOKUP = dict(LOSS)
+DRAW_LOOKUP = dict(DRAW)
+
 with open(os.path.dirname(sys.argv[0]) + "/input.txt", 'r') as f:
     data = f.read()
     data = data.split('\n') 
@@ -37,6 +47,26 @@ for d in data:
         ...
     else:
         score += 3
+
+    scores.append(score)
+
+print(sum(scores))
+
+
+# Pt 2
+scores = []
+for d in data:
+    score = 0
+
+    if d[1] == 'Z':
+        score += SHAPE_POINTS[WIN_LOOKIP[d[0]]]
+        score += 6
+    elif d[1] == 'Y':
+        score += SHAPE_POINTS[DRAW_LOOKUP[d[0]]]
+        score += 3
+    else:
+        score += SHAPE_POINTS[LOSS_LOOKUP[d[0]]]
+        score += 0
 
     scores.append(score)
 
