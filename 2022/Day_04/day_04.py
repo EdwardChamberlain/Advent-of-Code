d93f0ac365ff086fd64474a0c382b5801f2cf017
Edward Chamberlain <edward.chamberlain@me.com>
2022-12-04 20:58:17 +0000

    Day 04 Pt 2

--- 2022/Day_04/day_04.py
+++ 2022/Day_04/day_04.py
@@ -9,6 +9,12 @@ def convert_range_to_list(x: str) -> str:
     x = ','.join(map(y, x))
     return x
 
+def conv_int(x):
+    x = x.split('-')
+    x = list(map(int, x))
+    return x
+
+# Pt 1
 with open(os.path.dirname(sys.argv[0]) + "/input.txt", 'r') as f:
     data = f.read()
     data = data.split('\n') 
@@ -19,11 +25,24 @@ for n, d in enumerate(data):
     elf1 = convert_range_to_list(d[0])
     elf2 = convert_range_to_list(d[1])
 
-    print(elf1)
-    print(elf2)
-
     if elf1 in elf2 or elf2 in elf1:
         counter += 1
-        print(True)
 
 print(counter)
+
+# Pt 2
+count = 0
+for n, d in enumerate(data):
+    a = conv_int(d[0])
+    b = conv_int(d[1])
+    if a[0] < b[0]:
+        x = a
+        y = b
+    else:
+        x = b
+        y = a
+
+    if x[1] >= y[0]:
+        count += 1
+
+print(count)
