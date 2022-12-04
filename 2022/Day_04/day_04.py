74a6014627a18c52705b8dbca2075cca5cf49041
Edward Chamberlain <edward.chamberlain@me.com>
2022-12-04 21:14:01 +0000

    tidy up

--- 2022/Day_04/day_04.py
+++ 2022/Day_04/day_04.py
@@ -1,34 +1,36 @@
 import sys
 import os
 
-def convert_range_to_list(x: str) -> str:
-    x = x.split('-')
-    x = list(map(int, x))
-    x = list(range(x[0], x[1]+1))
-    y = lambda x: '.' + str(x) + '.'
-    x = ','.join(map(y, x))
-    return x
 
 def conv_int(x):
     x = x.split('-')
     x = list(map(int, x))
     return x
 
-# Pt 1
+
+def len_range(x):
+    return x[1] - x[0]
+
+
 with open(os.path.dirname(sys.argv[0]) + "/input.txt", 'r') as f:
     data = f.read()
     data = data.split('\n') 
     data = [d.split(',') for d in data]
 
-counter = 0 
-for n, d in enumerate(data): 
-    elf1 = convert_range_to_list(d[0])
-    elf2 = convert_range_to_list(d[1])
 
-    if elf1 in elf2 or elf2 in elf1:
-        counter += 1
+# Pt 1
+count = 0
+for a, b in data:
+    a = conv_int(a)
+    b = conv_int(b)
 
-print(counter)
+    mainlist, sub_list = (b, a) if len_range(a) < len_range(b) else (a, b)
+
+    if sub_list[0] >= mainlist[0] and sub_list[1] <= mainlist[1]:
+        count += 1
+
+print(count)
+
 
 # Pt 2
 count = 0
