4452a39c2ff01cd54a8926156a27327fc779c419
Edward Chamberlain <edward.chamberlain@me.com>
2022-12-05 21:26:39 +0000

    Day 5 Pt 1

---
+++ 2022/Day_05/day_05.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import re
+
+def move_crate(cols: list, from_location: int, to_location: int) -> list:
+    cols[to_location-1].append(cols[from_location-1].pop())
+    return cols
+
+with open(os.path.dirname(sys.argv[0]) + "/input.txt", 'r') as f:
+    data = f.read()
+    start_positions, moves = data.split('\n\n') 
+
+start_positions = start_positions.split('\n')
+start_positions.reverse()
+
+# Pt 1
+cols = []
+for n in range(9):
+    col = [sp[n*4 + 1] for sp in start_positions[1:] if sp[n*4 + 1] != ' ']
+    cols.append(col)
+
+moves = moves.split('\n')
+pattern = re.compile('move (.*) from (.*) to (.*)')
+moves = [pattern.match(m).groups() for m in moves]
+moves = [list(map(int, m)) for m in moves]
+
+for m in moves:
+    for i in range(m[0]):
+        cols = move_crate(cols, m[1], m[2])
+
+print(
+    ''.join([c[-1] for c in cols])
+)
