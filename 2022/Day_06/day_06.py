d334ffcdc59ab06e578df35a9be086f1b70dcaf2
Edward Chamberlain <edward.chamberlain@me.com>
2022-12-06 17:24:57 +0000

    var rename

--- 2022/Day_06/day_06.py
+++ 2022/Day_06/day_06.py
@@ -2,11 +2,11 @@ import sys
 import os
 
 
-def find_marker(data: str, lookahead: int) -> int:
+def find_marker_index(data: str, marker_size: int) -> int:
     for i in range(len(data)):
-        search_range = data[i:i+lookahead]
-        if len(set(search_range)) == lookahead:
-            return i+lookahead
+        search_range = data[i:i+marker_size]
+        if len(set(search_range)) == marker_size:
+            return i+marker_size
 
 
 with open(os.path.dirname(sys.argv[0]) + "/input.txt", 'r') as f:
@@ -14,10 +14,10 @@ with open(os.path.dirname(sys.argv[0]) + "/input.txt", 'r') as f:
 
 # Pt 1
 print(
-    find_marker(data, 4)
+    find_marker_index(data, 4)
 )
 
 # Pt 2
 print(
-    find_marker(data, 14)
+    find_marker_index(data, 14)
 )
